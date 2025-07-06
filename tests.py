87b598f852070385828bfc327db713663dc1dab8
Téa Kidder <135737177+jibawee@users.noreply.github.com>
2025-07-06 12:51:05 -0700

    Update tests.py

--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@ import unittest
 from credit_card_validator import credit_card_validator
 
 class TestCreditCardValidator(unittest.TestCase):
-6
+
     # VERIFY vALID CARD NUMBERS WORK
     # valid visa w prefix 3, length 16, and correct Luhn 
     def test_visa_prefix_4(self):
@@ -32,11 +32,11 @@ class TestCreditCardValidator(unittest.TestCase):
     def test_invalid_length(self):
         self.assertFalse(credit_card_validator("411111111111111"))       # Visa, 15 BUG FOUND
         self.assertFalse(credit_card_validator("41111111111111111"))     # Visa, 17
-        self.assertFalse(credit_card_validator("34000000000000"))        # AmEx, 14
-        self.assertFalse(credit_card_validator("3400000000000009"))      # AmEx, 16 BUG FOUND 
         self.assertFalse(credit_card_validator("550000000000000"))       # MasterCard, 15
         self.assertFalse(credit_card_validator("55000000000000044"))     # MasterCard, 17
-
+        self.assertFalse(credit_card_validator("34000000000000"))        # AmEx, 14
+        self.assertFalse(credit_card_validator("3400000000000009"))      # AmEx, 16 BUG FOUND 
+        
     # BAD LUHN DIGIT
     def test_invalid_luhn(self):
         self.assertFalse(credit_card_validator("4111111111111112"))  # Visa
